db64a68ce82ce35fd9d357cdca5ad969b5fecdce
Hoang <hoangnth0312@gmail.com>
2026-04-25 20:19:32 +0700

    Initial commit

---
+++ buoi10/Player.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace buoi10;
+
+public class Player
+{
+    public string name;
+    public int score;
+    public int life;
+}
+//thuoc tinh 
+public class name
+{
+}
---
+++ buoi10/Program.cs
@@ -0,0 +1,27 @@
+﻿//Lop va doi tuong
+
+
+//phuong thuc
+//cach viet phuong thuc:
+//ten pt + () {
+//cac cau lenh
+//}
+
+void tinhTong(int a, int b)
+{
+    int tong = a + b;
+    Console.WriteLine(tong);
+}
+
+//goi phuong thuc nhieu lan ma ko can code
+
+tinhTong(2,4);
+
+void TinhTich (int a, int b)
+{
+    int tich = a * b;
+    int soCuoi = tich * 3;
+    Console.WriteLine(tich);
+}
+TinhTich(3, 4);
+TinhTich(4, 5);
